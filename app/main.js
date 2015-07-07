bfb3667aa5652f23c2a60e9ab8812275f92e8e05
christianalfoni <christianalfoni@gmail.com>
2015-07-07 19:43:10 +0200

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Formsy = require('formsy-react');
+
+var App = React.createClass({
+  render() {
+    return (
+      <h1>Hello world!</h1>
+    );
+  }
+});
+
+ReactDOM.render(<App/>, document.body);
